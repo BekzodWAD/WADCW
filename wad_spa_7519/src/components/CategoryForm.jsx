094afe9e3286a73122525b161d33f50793cd92bb
Unknow-Googly <85851892+BekzodWAD@users.noreply.github.com>
2021-07-02 15:15:58 +0500

    Update CategoryForm.jsx

--- wad_spa_7519/src/components/CategoryForm.jsx
+++ wad_spa_7519/src/components/CategoryForm.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Formik, Form, Field } from 'formik';
-import { PostData } from '../utils/ajax';
+import { GetData, PostData, PutData } from '../utils/ajax';
 
-const CategoryForm = ({ history }) => {
+const CategoryForm = ({ history, match }) => {
     
+    const [formData, setFormData] = useState({
+        Name: "",
+        ColorCode: "Red"
+    })
+
+    useEffect(()=>{
+        if (match.params.categoryId) {
+            GetData(`/api/Categories/${match.params.categoryId}`)
+                .then(res => {
+                    const { name, colorCode, categoryId } = res;
+                    setFormData({
+                        CategoryId: categoryId,
+                        Name: name,
+                        ColorCode: colorCode
+                    })
+                })
+        }
+    }, [])
+
     const handleSubmit = values=>{
         try{
-            PostData("/api/Categories", values, history.goBack)
+            if(match.params.categoryId)
+                PutData(`/api/Categories/${match.params.categoryId}`, values, history.goBack)
+            else
+                PostData("/api/Categories", values, history.goBack)
         }catch(e){
             console.error(e);
         }
@@ -15,26 +37,28 @@ const CategoryForm = ({ history }) => {
     return (
         <div style={{width:'50%', margin: 'auto'}}>
             <h3>Create category</h3>
-            <Formik 
-                onSubmit={handleSubmit}
-                initialValues={{
-                    Name: "",
-                    ColorCode: "Red"
-                }}
-            >
-                <Form className="form-group">
-                    <label>Category name</label>
-                    <Field name="Name" className="form-control" />
-                    <label htmlFor="">Color</label>
-                    <Field name="ColorCode" as="select" className="form-control">
-                        <option value="red">Red</option>
-                        <option value="green">Green</option>
-                        <option value="blue">Blue</option>
-                    </Field>
+            {
+                (!(match.params.categoryId) || (match.params.categoryId && formData.CategoryId)) ?
+                    <Formik
+                        onSubmit={handleSubmit}
+                        initialValues={formData}
+                    >
+                        <Form className="form-group">
+                            <label>Category name</label>
+                            <Field name="Name" className="form-control" />
+                            <label htmlFor="">Color</label>
+                            <Field name="ColorCode" as="select" className="form-control">
+                                <option value="red">Red</option>
+                                <option value="green">Green</option>
+                                <option value="blue">Blue</option>
+                            </Field>
 
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </Form>
-            </Formik>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </Form>
+                    </Formik>
+                    :null
+            }
+            
         </div>
     )
 }
